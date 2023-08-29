56b952a8ad17cc9c31368c8a4a2af4bae378ba8e
Rob Prouse <r.prouse@valsoftcorp.com>
2023-08-29 14:08:42 -0400

    Starting a custom NvChad configuration

---
+++ chadrc.lua
@@ -0,0 +1,23 @@
+---@type ChadrcConfig 
+local M = {}
+M.ui = {
+  theme = 'vscode_dark',
+  nvdash = {
+    load_on_startup = true,
+    buttons = {
+      { "  Find File", "Spc f f", "Telescope find_files" },
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+      { "  Bookmarks", "Spc m a", "Telescope marks" },
+      { "󰙅  File Tree", "Spc e", "NvimTreeOpen" },
+      { "  Themes", "Spc t h", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+    },
+  },
+
+  statusline = {
+    theme = "vscode_colored",
+    separator_style = "default"
+  },
+}
+return M
